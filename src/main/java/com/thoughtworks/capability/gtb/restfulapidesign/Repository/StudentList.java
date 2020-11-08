0bad2dae120efb3c74d1967ce31cbf6ac7ba2d73
Yafeng Wang <yafeng.wang@wyf.local>
2020-11-08 11:44:47 +0800

    创建StudentsList Repository

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Repository/StudentList.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Repository/StudentList.java
@@ -1,4 +1,10 @@
 package com.thoughtworks.capability.gtb.restfulapidesign.Repository;
 
+import com.thoughtworks.capability.gtb.restfulapidesign.Dto.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentList {
+    public static List<Student> studentList = new ArrayList<Student>();
 }
